4f41b2abb396edb43d288209b4269818e4a785b5
Aman-Rajput8326 <shubhamkumar8326@gmail.com>
2024-12-19 23:00:48 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className="w-screen h-screen bg-richblack-900 flex flex-col">
       <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <Routes>
-        <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigate to="/login" />} />
         <Route
           path="/login"
           element={<Login setIsLoggedIn={setIsLoggedIn} />}
